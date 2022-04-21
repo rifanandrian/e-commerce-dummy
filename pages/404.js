22e3efa4f907b87073c3371243ac1b4fa2f68845
Andrian <muhamadrifanandrian@gmail.com>
2022-04-21 22:42:24 +0700

    fixing 404

--- pages/404.js
+++ pages/404.js
@@ -1,19 +1,17 @@
 import Link from 'next/link';
-import styles from '../styles/404.module.scss';
+import styles from '../styles/CustomError.module.scss';
 
 const Custom404ErrorPage = () => {
   return (
-    <>
-      <div className={styles.content}>
-        <div className={styles.title}>404</div>
-        <div className={StyleSheet.small_text}>
-          <span className="">Take me back to &nbsp;</span>
-          <Link href="/">
-            <a className="">store</a>
-          </Link>
-        </div>
+    <div className={styles.content}>
+      <div className={styles.title}>404</div>
+      <div className={styles.small_text}>
+        <span className="">Take me back to &nbsp;</span>
+        <Link href="/">
+          <a className="">store</a>
+        </Link>
       </div>
-    </>
+    </div>
   );
 };
 
